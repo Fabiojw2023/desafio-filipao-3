7750fb5b76acb66fc168bee161d0a48a55b10eb6
Fabiojw2023 <fssjw2018@gmail.com>
2023-11-03 14:31:08 -0300

    projeto criado

---
+++ index.js
@@ -0,0 +1,20 @@
+class heroi{
+   constructor(nome, idade, tipo){
+    this.nome = nome
+    this.idade = idade
+    this.tipo = tipo
+   } 
+} 
+
+switch (true){
+    case "homem de ferro" === "homem de ferro";
+        'return "Uso de plasma"
+    case "guerreiro" === "guerreiro";
+        return ""
+    case "monge" === "monge";
+        return ""
+    case "nija" === "nija";
+    return ""
+        
+    }
+}        
