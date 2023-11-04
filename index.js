a62fc31f08e77ae940da3f48219373d37e5edaad
Fabiojw2023 <fssjw2018@gmail.com>
2023-11-04 07:34:28 -0300

    atualização

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-class heroi{
-   constructor(nome, idade, tipo){
-    this.nome = nome
-    this.idade = idade
-    this.tipo = tipo
-   } 
-} 
-
-switch (true){
-    case "homem de ferro" === "homem de ferro";
-        'return "Uso de plasma"
-    case "guerreiro" === "guerreiro";
-        return ""
-    case "monge" === "monge";
-        return ""
-    case "nija" === "nija";
-    return ""
-        
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
     }
-}        
+
+    escrever() {
+        console.log("Nome: " + this.nome + ", Idade: " + this.idade + ", Tipo: " + this.tipo);
+    }
+}
+
+let heroi1 = new Heroi("Fábio", 33, "homem de ferro");
+heroi1.escrever(); 
+
+let tipo = "homem de ferro";
+
+switch (tipo) {
+    case "homem de ferro":
+        console.log("O " + tipo + " atacou usando plasma");
+        break;
+
+    case "guerreiro":
+        console.log("O " + tipo + " atacou usando espada");
+        break;
+
+    case "monge":
+        console.log("O " + tipo + " atacou usando artes marciais");
+        break;
+
+    case "ninja":
+        console.log("O " + tipo + " atacou usando shuriken");
+        break;
+
+    default:
+        console.log("O " + tipo + " habilidade desconhecida");
+}
